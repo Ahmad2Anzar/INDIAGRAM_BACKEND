5ac03ae6ef667cd24b47cad366fb8aaef118967f
Ahmad2Anzar <ahmad2anzar@gmail.com>
2024-10-10 16:24:57 +0530

    Adding PORT to env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port =process.env.PORT|| 8000;
 require('dotenv').config();
 const session = require('express-session');
 const mongoDB = require('./db');
